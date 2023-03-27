a8833ee42fb8ffb5e035d88d55e66916b299da1c
ShashikantYadav0111 <yadavshashikant43@gmmail.com>
2023-03-27 13:43:23 +0530

    Updates

---
+++ precache-manifest.e497b5beb01da72f592c9ea0a9087465.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aac434b1a8d7aed3878b",
+    "url": "/ShashikantYadav0111/voicetotext/static/css/main.5eb1b7b3.chunk.css"
+  },
+  {
+    "revision": "aac434b1a8d7aed3878b",
+    "url": "/ShashikantYadav0111/voicetotext/static/js/main.aac434b1.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/ShashikantYadav0111/voicetotext/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "a0ebc900f58178b621b2",
+    "url": "/ShashikantYadav0111/voicetotext/static/js/2.a0ebc900.chunk.js"
+  },
+  {
+    "revision": "1aeaf404929f66953bf1",
+    "url": "/ShashikantYadav0111/voicetotext/static/js/runtime~main.1aeaf404.js"
+  },
+  {
+    "revision": "371c477dfabe4f410a1696d567bb53c5",
+    "url": "/ShashikantYadav0111/voicetotext/index.html"
+  }
+];
